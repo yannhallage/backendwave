56f8dc33ca1ccb622511a4de1e0b8bf70f437d04
yannhallage <contact.devhllg@gmail.com>
2025-06-09 23:38:52 +0000

    securitsation des routes api 23:38

--- routes/myaccountwave.route.js
+++ routes/myaccountwave.route.js
@@ -1,17 +1,19 @@
 const express = require('express')
+const verifyToken = require('../middleware/authwave.middleware')
+
 const route_TransactionAccount = express.Router();
 const { GetAccountWave, getAllTransactionsForUser, UpdateServerRechargeAccount, GetAllAccount, CreateTransaction, DeleteAllTransaction, GetReception } = require('../controllers/myaccountwave.controller')
 
 
 // ici on gere les Auths
 route_TransactionAccount.get('/', GetAllAccount)
-route_TransactionAccount.post('/transactions', CreateTransaction);
-route_TransactionAccount.get('/transactions/:numeroTel', getAllTransactionsForUser)
+route_TransactionAccount.post('/transactions', verifyToken, CreateTransaction);
+route_TransactionAccount.get('/transactions/:numeroTel', verifyToken, getAllTransactionsForUser)
 // route_TransactionAccount.get('/transactions/:id', GetAllTransactions)
-route_TransactionAccount.get('/reception/:id', GetReception)
-route_TransactionAccount.get('/:id', GetAccountWave)
-route_TransactionAccount.post('/:id', GetAccountWave)
-route_TransactionAccount.put('/Erecharge', UpdateServerRechargeAccount)
+route_TransactionAccount.get('/reception/:id', verifyToken, GetReception)
+route_TransactionAccount.get('/:id', verifyToken, GetAccountWave)
+// route_TransactionAccount.post('/:id', GetAccountWave)
+route_TransactionAccount.put('/Erecharge', verifyToken, UpdateServerRechargeAccount)
 route_TransactionAccount.delete('/suppressions', DeleteAllTransaction)
 
 module.exports = route_TransactionAccount;
