abbab4cff95a9aae5d6bf4ae253516cfde5667b8
thisisyannhallage <contact.devhllg@gmail.com>
2025-06-13 01:42:11 +0000

    Update myaccountwave.route.js

--- routes/myaccountwave.route.js
+++ routes/myaccountwave.route.js
@@ -2,7 +2,9 @@ const express = require('express')
 const verifyToken = require('../middleware/authwave.middleware')
 
 const route_TransactionAccount = express.Router();
-const { GetAccountWave, getAllTransactionsForUser, UpdateServerRechargeAccount, GetAllAccount, CreateTransaction, DeleteAllTransaction, GetReception } = require('../controllers/myaccountwave.controller')
+const { GetAccountWave, getAllTransactionsForUser, UpdateServerRechargeAccount, 
+       GetAllAccount, 
+       CreateTransaction, DeleteAllTransaction, GetReception } = require('../controllers/myaccountwave.controller')
 
 
 // ici on gere les Auths
